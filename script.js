4a11d4e3d11b31f6bebb295b5ced4f7558b6e3db
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2026-01-14 22:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+// Initial array
+const arr = [1, 2, 3, 4];
+
+// Initial Promise (resolves after 3 seconds with array)
+function initialPromise() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  });
+}
+
+// Start chained promises
+initialPromise()
+  .then(numbers => {
+    // Filter even numbers
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+    // Return a promise with 1 second delay
+    return new Promise(resolve => {
+      setTimeout(() => {
+        output.innerText = evenNumbers.toString(); // "2,4"
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then(evenNumbers => {
+    // Multiply even numbers by 2
+    const multiplied = evenNumbers.map(num => num * 2);
+
+    // Return a promise with 2 second delay
+    return new Promise(resolve => {
+      setTimeout(() => {
+        output.innerText = multiplied.toString(); // "4,8"
+        resolve(multiplied);
+      }, 2000);
+    });
+  });
